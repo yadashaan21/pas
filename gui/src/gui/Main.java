a2a6b32249f50fecb2050ff4b50aab238b6021f3
B P YADAV <shaanyadav160@gmail.com>
2025-02-04 22:04:09 +0530

    fifth commit

---
+++ gui/src/gui/Main.java
@@ -0,0 +1,15 @@
+package gui;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name+"!");
+        
+        int age= Integer.parseInt(JOptionPane.showInputDialog("enter your age"));
+        JOptionPane.showMessageDialog(null,"You are "+age+" year old.");
+        
+        double height= Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null,"You are "+height+" cm tall.");
+    }
+}
